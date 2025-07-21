97c9e73b1f166aeb490635279b733ab115dc7601
iamscob <alexeyka0124@gmail.com>
2025-07-21 11:46:45 +0200

    Добавлен проект Tanks and Turrets

---
+++ Tanks and Turrets/Source/ToonTanks/BasePawn.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+ABasePawn::ABasePawn()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider")); // Building the CapsuleComp
+	RootComponent = CapsuleComp;																		  // Attaching as root component
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh")); // Building the BaseMesh
+	BaseMesh->SetupAttachment(CapsuleComp);												 // Attaching to component (root: CapsuleComp)
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+	TurretMesh->SetupAttachment(BaseMesh);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Spawn Point"));
+	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
+}
+// Called when the game starts or when spawned
+void ABasePawn::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ABasePawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ABasePawn::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
